cdf7e98010f98cee498bec1a99035e4a3835c919
Annam Iyer <annam.iyer@vanderbilt.edu>
2018-01-30 22:49:45 -0600

    started adding UI

---
+++ Waterboy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Waterboy
+//
+//  Created by Annam Iyer on 1/30/18.
+//  Copyright © 2018 Annam Iyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
